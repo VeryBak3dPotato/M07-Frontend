5bff543e1e6c8c8bccebe71e0d10c1b260685d94
Connor Bailey <cebailey92@gmail.com>
2025-03-03 14:11:33 -0500

    changed back

--- scripts/auth.js
+++ scripts/auth.js
@@ -6,8 +6,8 @@ class Auth {
     };
     validateAuth(auth) {
         if (auth != 1) {
-            window.location.replace("/M07-Frontend/login.html");
-            // window.location.replace("/login.html");
+            // window.location.replace("/M07-Frontend/login.html");
+            window.location.replace("/login.html");
         } else {
             document.querySelector("body").style.display = "block";
         }
@@ -16,7 +16,7 @@ class Auth {
         localStorage.removeItem("auth");
         localStorage.removeItem("token");
         localStorage.removeItem("uname");
-        window.location.replace("/M07-Frontend/login.html");
-        // window.location.replace("/login.html");
+        // window.location.replace("/M07-Frontend/login.html");
+        window.location.replace("/login.html");
     }
 }
